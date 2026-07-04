8987d12ffc75c680fea56a7a504c2c16efd212fc
arslan <arslancs5065@gmail.com>
2026-07-04 20:46:14 +0500

    dist actually remvoed

--- backend/dist/utils/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-/**
- * Wraps an async route handler and forwards rejected promises to next().
- */
-const asyncHandler = (fn) => (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-};
-export default asyncHandler;
-//# sourceMappingURL=asyncHandler.js.map
